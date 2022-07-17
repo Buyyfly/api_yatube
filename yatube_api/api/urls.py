af4467198ec293ecf41acbde33a6119702480367
Buyy <Buyyfly@gmail.com>
2022-07-17 15:07:36 +0300

    v4

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import include, path
+import djoser.urls.jwt
+import djoser.urls
 from .views import PostViewSet, GroupViewSet, CommentViewSet
 
 
 router = SimpleRouter()
 router.register('posts', PostViewSet, basename='posts')
-router.register('group', GroupViewSet, basename='groups')
+router.register('groups', GroupViewSet, basename='groups')
 router.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
@@ -14,6 +16,6 @@ router.register(
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include(djoser.urls)),
+    path('auth/', include(djoser.urls.jwt)),
 ]
